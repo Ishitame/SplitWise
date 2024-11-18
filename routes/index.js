89b6c289931d6eda8c2a0035d9f1327d4de1d0b3
kushagra patidar <kushagraptdr18@gmail.com>
2024-11-18 23:59:47 +0530

    Changes Kushagra

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,9 @@
 const express = require('express');
 const route = express.Router();
 
+route.get("/", function(req, res){
+    res.send("Hello, this is the Group page");
+})
+
+
 module.exports = route;
